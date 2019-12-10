f1b754fdd0f6d30fd5814c62937fef6b3e22cfd1
Luyin <myraous@gmail.com>
2019-12-10 13:57:54 +0900

    fix: dateTime 초기화 이슈 수정

--- src/components/consultation/MemoEditor.js
+++ src/components/consultation/MemoEditor.js
@@ -40,6 +40,21 @@ export default class MemoEditor extends Component {
 
             this.setState(newState);
         }
+
+        window.$('.datetimepicker').datetimepicker({
+            format: 'YYYY/MM/DD h:mm A',
+            icons: {
+              time: "fa fa-clock-o",
+              date: "fa fa-calendar",
+              up: "fa fa-chevron-up",
+              down: "fa fa-chevron-down",
+              previous: 'fa fa-chevron-left',
+              next: 'fa fa-chevron-right',
+              today: 'fa fa-screenshot',
+              clear: 'fa fa-trash',
+              close: 'fa fa-remove'
+            }
+        });
     }
 
     _onClickRegister = (e) => {
